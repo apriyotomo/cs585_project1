20214418f30bbf5b13bdd18b9fe71ee0a5a950d4
CongyuanTang <tangcongyuan@foxmail.com>
2015-02-08 23:00:37 -0500

    Added comments.

--- src/DataGenerator.java
+++ src/DataGenerator.java
@@ -3,8 +3,14 @@ public class DataGenerator {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		//CustomerDataGenerator CDG = new CustomerDataGenerator(50000);
-		//CDG.generate();
+		/* 
+		 * This file is for WPI CS585 Project 1.
+		 * Run the file to generate random customer and transaction data.
+		 * The customer data will be less than 2MB;
+		 * the transaction data will be around 300MB, be aware!
+		 * */
+		CustomerDataGenerator CDG = new CustomerDataGenerator(50000);
+		CDG.generate();
 		TransactionDataGenerator TDG = new TransactionDataGenerator(5000000,50000);
 		TDG.generate();
 	}
